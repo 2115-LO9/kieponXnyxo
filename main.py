094238cb0e28372bd02979daaaf9ac3b8e304edc
G1Nyxo <70384254+G1Nyxo@users.noreply.github.com>
2020-09-11 21:35:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+#biblioteki
+import os
+import time
+
+clear = lambda: os.system('cls')
+### definicja clear()
+
+print("Witaj w programie bez nazwy")
+print("Wchodzac do programu akceptujesz zasady przechowywania danych")

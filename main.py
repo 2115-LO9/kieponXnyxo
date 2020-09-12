07d945a8d0e50f9919c53cf02ae3a8ae850b311f
Tajmun <tymun.j102@gmail.com>
2020-09-12 11:42:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import time
+import os
+clear = lambda: os.system('cls')
+
+print("Oto program bez nazwy")
+imie=input("Tutaj możesz wpisac swoje imie:")
+if imie == (str):
+    print("Imie zosatało zapisane")
+else:
+    print("Nie można wpisywać liczb")
+time.sleep(3)
+clear()

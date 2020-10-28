8b2cdd0f7b8d69c5f5fbe98e33cad841aeb0460d
Tajmun <tymun.j102@gmail.com>
2020-10-28 14:34:50 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,44 @@
 import os
-
+import time
 clear = lambda: os.system('cls')
+while 1:
+    print("Witaj, w programie bez nazwy")
+    print("1-zaloguj się")
+    print("2-zarejestruj się")
+    print("3-zamknij program")
+    odp1=input()
+    clear()
+    time.sleep(1)
+    if odp1 == 1:
+        plik = open("bazadanych.txt",'r+')
+        odp2=input("Podaj login: ")
+        if x!= odp2:
+            print("Nie poprawny login")
+        elif x==odp2:
+            odp3=input("Podaj hasło:")
+            if y==odp2:
+                break
+    elif odp1==2:
+        while 1:
+            baza.v2={x:login,y:hasło}
+            odp1=input("Podaj login: ")
+            if odp1 in bazadanych.txt:
+                print("Login zajęty")
+                clear()
+                time.sleep(1)
+            else:
+                plik=write("bazadanych.txt",'a+')
+                break
+            while 1:
+                    odp2=input("Podaj hasło: ")
+                    odp3=input("Podaj hasło ponownie: ")
+                    if odp2 == odp3:
+                        print("Zostałeś zarejetrowany")
+                    else:
+                        print("Hasła nie są takie same")
+    elif odp1==3:
+            plik.close()
+            break
+    else:
+        print("Nie ma takiej opcji")
 
-
-print("Testowa baza danych")
-x=input("Podaj swój login")
-imie=input("A teraz wpisz swoje imie")
-testowabaza = {x: imie}
-clear()
-print("Jeśli chcesz wczytać listę imion podaj swój login ")
-loginTest= input()
-if loginTest == x:
-    print(testowabaza[x])
